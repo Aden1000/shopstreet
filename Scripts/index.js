09914ef6f43f6ba2cd22d67ff7201ab7d51db082
Aden1000 <enehizenaadriel55@gmail.com>
2021-07-28 11:27:16 +0300

    Initial commit

---
+++ Scripts/index.js
@@ -0,0 +1,6 @@
+document.getElementById("BannerMenu").onclick=function(){
+  $("#HeaderTabs").toggleClass("show");
+  $("#BannerMenu #Bar1").toggleClass("clicked");
+  $("#BannerMenu #Bar2").toggleClass("clicked");
+  $("#BannerMenu #Bar3").toggleClass("clicked");
+}
