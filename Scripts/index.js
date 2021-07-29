0d8b249688337648bdcd0d698e19400fd4bd55c5
Aden1000 <enehizenaadriel55@gmail.com>
2021-07-30 01:15:22 +0300

    Upload of website files

--- Scripts/index.js
+++ Scripts/index.js
@@ -1,6 +1,38 @@
-document.getElementById("BannerMenu").onclick=function(){
-  $("#HeaderTabs").toggleClass("show");
-  $("#BannerMenu #Bar1").toggleClass("clicked");
-  $("#BannerMenu #Bar2").toggleClass("clicked");
-  $("#BannerMenu #Bar3").toggleClass("clicked");
+document.getElementById("HeaderMenu").onclick=function(){
+  //create a smooth animation of the menu bars when clicked
+  $("#HeaderMenu #Bar1").toggleClass("clicked");
+  $("#HeaderMenu #Bar2").toggleClass("clicked");
+  $("#HeaderMenu #Bar3").toggleClass("clicked");
+  if($("#HeaderMenu #Bar1").hasClass("clicked")){
+     $("#HeaderMenu #Bar1").css("transform","translateY(10px)");
+    setTimeout(function(){ 
+      $("#HeaderMenu #Bar1").css("transform","translateY(10px) rotate(-45deg)");
+    },250);
+    
+     }
+  else{
+       $("#HeaderMenu #Bar1").css("transform","translateY(0px) rotate(0deg)");
+  }
+  if($("#HeaderMenu #Bar3").hasClass("clicked")){
+     $("#HeaderMenu #Bar3").css("transform","translateY(-10px)");
+    setTimeout(function(){ 
+      $("#HeaderMenu #Bar3").css("transform","translateY(-10px) rotate(45deg)");
+    },250);
+    
+     }
+  else{
+       $("#HeaderMenu #Bar3").css("transform","translateY(0px) rotate(0deg)");
+  }
+   $("#MainContent").toggleClass("slide");
+  $("#Header").toggleClass("slide");
+}
+
+document.body.onload=function(){
+  $("#HeaderExtra").height($("#Header").height());
+ $("#HeaderExtra").css("padding",$("#Header").css("padding"));
+}
+
+window.onresize=function(){
+   $("#HeaderExtra").height($("#Header").height());
+ $("#HeaderExtra").css("padding",($("#Header").css("padding")));
 }
